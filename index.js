ee454d324bd5ed23ef3e2c6dfba7c0f0a6112982
festus-sulumeti <sulumetifestus@outlook.com>
2023-09-17 08:40:23 +0300

    Completd assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+document.querySelector('main').remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+const champ = document.createElement('p');
+champ.innerHTML = 'YOUR-NAME is the champion';
+
+newHeader.appendChild(champ)
